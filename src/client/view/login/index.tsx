196a34b065acfdd4f37dbf2677e5530554f6db60
bezgin.i <kurt1v@yandex.ru>
2020-11-08 01:27:56 +0300

    добавлена страница логина

--- src/client/view/login/index.tsx
+++ src/client/view/login/index.tsx
@@ -1,3 +1,83 @@
+import { Layout, Typography } from "antd";
+import { Formik } from "formik";
+import React from "react";
+import * as FormikAntd from "formik-antd";
+import { SC } from "./styled";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
+const { Title } = Typography;
+
 export const LoginPage = React.memo(() => {
-    return <>Login Page</>;
+    return (
+        <Layout>
+            <Layout className="site-layout">
+                <SC.LoginForm>
+                    <h2 style={{ textAlign: "center" }}>Service.Auto</h2>
+
+                    <Formik
+                        initialValues={{
+                            username: "",
+                            password: "",
+                        }}
+                        onSubmit={() => {
+                            // void
+                        }}
+                    >
+                        {() => {
+                            return (
+                                <FormikAntd.Form>
+                                    <FormikAntd.Form.Item
+                                        name="username"
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message:
+                                                    "Please input your username!",
+                                            },
+                                        ]}
+                                    >
+                                        <FormikAntd.Input
+                                            prefix={
+                                                <UserOutlined className="site-form-item-icon" />
+                                            }
+                                            name="username"
+                                            placeholder="Username"
+                                        />
+                                    </FormikAntd.Form.Item>
+                                    <FormikAntd.Form.Item
+                                        name="password"
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message:
+                                                    "Please input your password!",
+                                            },
+                                        ]}
+                                    >
+                                        <FormikAntd.Input
+                                            prefix={
+                                                <LockOutlined className="site-form-item-icon" />
+                                            }
+                                            name="password"
+                                            placeholder="Password"
+                                        />
+                                    </FormikAntd.Form.Item>
+
+                                    <FormikAntd.Form.Item name="submit">
+                                        <FormikAntd.SubmitButton
+                                            type="primary"
+                                            htmlType="submit"
+                                            className="login-form-button"
+                                        >
+                                            Submit
+                                        </FormikAntd.SubmitButton>
+                                    </FormikAntd.Form.Item>
+                                </FormikAntd.Form>
+                            );
+                        }}
+                    </Formik>
+                </SC.LoginForm>
+            </Layout>
+        </Layout>
+    );
 });
