ee7ba517868a86d5cca3090812c1177e87e9d873
bezgin.i <kurt1v@yandex.ru>
2020-12-02 23:46:56 +0300

    фикс эслинт

--- config/jest/cssTransform.js
+++ config/jest/cssTransform.js
@@ -1,16 +1,16 @@
-/*! © CREATE-REACT-APP */
+/* ! © CREATE-REACT-APP */
 
-'use strict';
+"use strict";
 
 // This is a custom Jest transformer turning style imports into empty objects.
 // http://facebook.github.io/jest/docs/tutorial-webpack.html
 
 module.exports = {
     process() {
-        return 'module.exports = {};';
+        return "module.exports = {};";
     },
     getCacheKey() {
         // The output is always the same.
-        return 'cssTransform';
+        return "cssTransform";
     },
 };
